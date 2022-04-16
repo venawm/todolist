81c690f70381b511697d09a2142b4879db42304d
Samir <pandeysamir529@gmail.com>
2022-04-16 13:37:29 +0545

    React Todolist app

---
+++ src/components/input.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Input= () => {
+    return (
+        <div>
+            <form action="">
+                <input type="text" />
+            </form>
+
+            
+        </div>
+    );
+};
+
+export default Input
